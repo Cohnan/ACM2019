b59afef200e60b78438a8ec5caf6bab456bb3136
Cohnan <sc.puerto10@uniandes.edu.co>
2019-01-30 10:47:11 -0500

    Mas sobre javascript y node.js

--- Summary Node.js
+++ Summary Node.js
@@ -9,3 +9,16 @@ Concept importante: Modulos
 Encapsular clasesy otras cosas. Algo asi como librerias, pero internas de node.
 
 
+Ventajas de node.js
+
+Es asincrono (forma de manejar peticiones)
+
+Sincrono vs Asincrono (forma de manejar peticiones): si mi aplicaciones es un mesero:
+
+Sincrono: 1 proceso a la vez. Si me hacen un pedido, no paso al siguiente pedido hasta que la orden este completada (se haya entregado el plato de mi orden actual).
+
+Asincrono: mientras se cocina (concluye) una orden, responde a otra solicitud. Perfecto para el servidor.
+
+Ventaja: si el programa require mucha CPU es de manera sincrona
+
+
--- logger.js
+++ logger.js
@@ -1,12 +1,20 @@
+// Importar clase para manejo de eventos
+const emisorEventos = require('events')   // La consante es una clase
+
 var url = 'https://holamundo.co';
 
-// Crear modulo
-function log(message)
-{
-    console.log(message)
+Class Logger extends emisorEventos {}
+    log(message) // Una funcion dentro de una clase no es function, se llama metodo y no dice nada
+    {
+        console.log(message)
+    }
 }
 
 // Exportar la funcion log y la variable url, PERO con los nombres funcion y variable respectivamente
-module.exports.funcion = log;
+//module.exports.funcion = log;
+//module.exports.variable = url;
 
-module.exports.variable = url;
+
+// Emitir eventos
+emisor.emit('Mensaje recibido', 'otro', 'yotro'); // Puedo enviar varios mesajes, llegan como argumentos
+emisor.emit('Mensaje recibido', {id:1, type: 'http'}); // Un json como siguiente parametro
